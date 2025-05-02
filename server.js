bf0e474062bae5983011b16aad095771c7578821
Glitch (glitch-blank-node) <none>
2025-05-02 02:41:36 +0000

    🏘📍 Checkpoint ./public/main.css:37123583/541 ./server.js:37123583/42 ./src/dictionary.json:37123583/112 ./src/lessons.json:37123583/12

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ handlebars.registerHelper('hover-translate', function(arg, lang, options) {
       }
       else continue;
       
-      let construction = `<div class="hint"><span>${save}</span><table class="hints"><tbody>`;
-      for (var key of keys) { construction += `<tr class="row"><td class="row-whole" colspan="${submeaning !== [] ? submeaning.length : 1}">${key}</td></tr>`; }
+      let construction = `<div class="hint"><span>${save}</span><table><tbody>`;
+      for (var key of keys) { construction += `<tr class="row"><td colspan="${submeaning !== [] ? submeaning.length : 1}">${key}</td></tr>`; }
       if (submeaning !== []) {
         for (let i = 0; i < getLongestList(submeaning).length; i++) {
           construction += `<tr>`;
@@ -60,9 +60,7 @@ handlebars.registerHelper('hover-translate', function(arg, lang, options) {
 
 function getLongestList(nestedList) {
   let largest = [];
-  nestedList.forEach(element => {
-    if (element.length > largest.length) largest = element;
-  });
+  nestedList.forEach(element => { if (element.length > largest.length) largest = element; });
   return largest;
 }
 
