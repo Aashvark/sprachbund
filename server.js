8d813813a51d2f64bdbc4ba1b2321c1d2271aed3
Glitch (glottis) <none>
2025-05-09 01:11:53 +0000

    👛🐉 Checkpoint ./src/lessons.json:37123583/4 ./server.js:37123583/611 ./public/learn.css:37123583/380 ./public/main.css:37123583/3

--- server.js
+++ server.js
@@ -66,18 +66,12 @@ const lessons = require("./src/lessons.json");
 const dictionary = require("./src/dictionary.json");
 
 fastify.get("/", function (request, reply) { return reply.view("/src/index.hbs", { seo: seo.index, units: lessons }); });
+fastify.get("/lesson", function (request, reply) { return reply.redirect('/'); });
+fastify.post("/lesson", function (request, reply) { return reply.view('/src/lesson.hbs', {seo: seo, unitno: request.body.unit, lessons: lessons[request.body.unit].unit[request.body[request.body.unit + "-lesson"]]});});
+
 fastify.setNotFoundHandler(function(request, reply) { return reply.view("/src/error.hbs", { seo: seo.index, error: request.routeOptions.url }); });
 
-fastify.get("/lesson", function (request, reply) {
-  return reply.redirect('/');
-});
-
-fastify.post("/lesson", function (request, reply) {
-  return reply.view('/src/lesson.hbs', {seo: seo, unitno: request.body.unit, lessons: lessons[request.body.unit].unit[request.body[request.body.unit + "-lesson"]]});
-});
-
-fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
+fastify.listen({ port: process.env.PORT, host: "0.0.0.0" },
   function (err, address) {
     if (err) {
       console.error(err);
