7f39278672af4f650b4a51f8fa85dd7d022e21f2
Glitch (glottis) <none>
2025-06-07 22:26:02 -0400

    update a

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ function hoverForeign(tokens) {
     let token = tokens[index];
 
     for (var str of token) {
-      console.log(stored + ":" + stored.trimEnd() === stored);
+      console.log(stored.trimEnd() + ":" + stored + ":");
       if (stored.trimEnd() === stored) stored = "";
       stored += str;
 
