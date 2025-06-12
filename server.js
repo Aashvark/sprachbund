9d87212885397d08b0a1af8534a153b179f65b00
Glitch (glottis) <none>
2025-06-11 21:26:36 -0400

    update a

--- server.js
+++ server.js
@@ -9,16 +9,15 @@ const dictionary = require("./src/json/dictionary.json");
 let dict = dictionary.nórem;
 
 fastify.register(require("@fastify/static"), { root: path.join(__dirname, "public"), prefix: "/" });
-
-fastify.register(require("@fastify/formbody"));
 fastify.register(require("@fastify/view"), { engine: { handlebars: handlebars } });
+fastify.register(require("@fastify/formbody"));
 
 handlebars.registerHelper('add', function(a, b) { return a + b; });
 handlebars.registerHelper('ifEquals', function(arg1, arg2, options) { return (arg1 === arg2) ? options.fn(this) : options.inverse(this); });
 handlebars.registerHelper('ternaryEq', function(arg1, arg2, op1, op2) { return (arg1 === arg2) ? op1 : op2; });
 handlebars.registerHelper('json', function(a) { return JSON.stringify(a); });
 handlebars.registerHelper('get-attribute', function(word, attribute) { return dict[word.trimEnd()][attribute]; });
-handlebars.registerHelper('tip-format', function(arg) { return new handlebars.SafeString(arg.replace("[", "<span class=\"merienda accent\">").replace("]", "</span>")); });
+handlebars.registerHelper('tip-format', function(arg) { return new handlebars.SafeString(replaceClass(arg, "merienda accent")); });
 
 handlebars.registerHelper('hover-translate', function(prompt, lang) {
   // han i mikol. => [["han"], ["i"], ["mikol", "."]]
@@ -44,7 +43,7 @@ function hoverNative(tokens) {
     let next = parseInt(index) + 1;
     stored += token[0];
 
-    if (index < tokens.length - 1 && !getInComplexByLength(stored + " " + tokens[next][0])) stored += " ";
+    if (index < tokens.length - 1 && isInDictionary(stored + " " + tokens[next][0]) && token.length === 1) stored += " ";
     else {
       let submeaning;
       if (stored.includes(" ")) { submeaning = stored.split(" ").map((v) => { return Object.keys(dict).filter(key => dict[key].simple.includes(v)); }); }
@@ -64,7 +63,7 @@ function hoverForeign(tokens) {
     let next = parseInt(index) + 1;
     stored += token[0];
 
-    if (index < tokens.length - 1 && (stored + " " + tokens[next][0]).toLowerCase() in dict) stored += " ";
+    if (index < tokens.length - 1 && (stored + " " + tokens[next][0]).toLowerCase() in dict && token.length === 1) stored += " ";
     else {
       let submeaning;
       if (stored.includes(" ")) { submeaning = stored.split(" ").map((key) => { return key in dict ? dict[key].simple : []; }); }
@@ -95,74 +94,7 @@ function formHints(word, keys, submeaning) {
 
 function replaceClass(text, class_) { return text.replaceAll("[", `<span class=\"${class_}\">`).replaceAll("]", "</span>"); }
 
-// handlebars.registerHelper('hover-translates', function(arg, lang) {
-//   let string = "";
-//   let save = "";
-  
-//   let tokens = [];
-//   for (let i = 0; i < arg.length; i++) {
-//     if (arg[i] === "." || arg[i] === " ") { 
-//       tokens.push(save);
-//       save = "";
-//     }
-//     if (arg[i] != " ") save += arg[i];
-//   }
-//   if (save !== "") tokens.push(save);
-//   save = "";
-
-//   let keys = undefined;
-//   let submeaning = [];
-  
-//   for (let i = 0; i < tokens.length; i++) {
-//     save += tokens[i] + " ";
-    
-//     if (tokens[i] === "___") {
-//       string += `<div class="hint"><hr class="blank"></div>`;
-//       save = "";
-//       keys = undefined;
-//       submeaning = [];
-//       continue;
-//     } else if (tokens[i] === ".") {
-//       string += `<div class="punctuation">${tokens[i]}</div>`;
-//       save = "";
-//       keys = undefined;
-//       submeaning = [];
-//       continue;
-//     } else if (lang === "native" && getInComplexByLength(save.toLowerCase() + tokens[i + 1]) && getInComplexByLength(save.toLowerCase() + tokens[i + 1] + " " + tokens[i + 2])) {
-//       keys = Object.keys(dict).filter(key => getInComplex(key, save.trimEnd().toLowerCase()));
-//       if (save.trimEnd().includes(" ")) { submeaning = save.trimEnd().split(" ").map((v) => { return Object.keys(dict).filter(key => dict[key].simple.includes(v)); }); }
-//     } else if (lang === "foreign" && save.trimEnd() in dict && !(save.toLowerCase() + tokens[i + 1] in dict) && !(save.toLowerCase() + tokens[i + 1] + " " + tokens[i + 2] in dict)) {
-//       keys = dict[save.trimEnd().toLowerCase()].simple;
-//       if (save.trimEnd().includes(" ")) { submeaning = save.toLowerCase().trimEnd().split(" ").map((key) => { return key in dict ? dict[key].simple : []; }); }
-//     } else if (lang === "foreign" && save[0] === save[0].toUpperCase()) {} else continue;
-    
-//     let construction;
-//     if (keys == undefined || keys.length >= 1) {
-//       construction = `<div class="hint"><span>${save.trimEnd(" ")}</span><table><tbody>`;
-//       for (var key of keys) { construction += `<tr class="row"><td colspan="${submeaning.length > 0 ? submeaning.length : 1}">${key}</td></tr>`; }
-//       if (submeaning.length > 0) {
-//         for (let i = 0; i < getLongestList(submeaning).length; i++) {
-//           construction += `<tr>`;
-//           for (var sub of submeaning) { construction += `<td>${sub.length > i ? sub[i] : ""}</td>`; } 
-//           construction += `</tr>`;
-//         }
-//       }
-//       construction += `</tbody></table></div>`;
-//     } else { construction = `<div class="hint">${save.trimEnd(" ")}</div>`; }
-      
-//     string += construction;
-//     save = "";
-//     keys = undefined;
-//     submeaning = [];
-//   }
-  
-//   return new handlebars.SafeString(string.trimEnd());
-// });
-
-function getInComplexByLength(match) {
-  console.log(match);
-  return Object.keys(dict).filter(key => dict[key].complex.includes(match.toLowerCase())).length === 0; }
-
+function isInDictionary(match) { return Object.keys(dict).filter(key => dict[key].complex.includes(match.toLowerCase())).length !== 0; }
 function getInComplex(key, match) { return dict[key].complex.includes(match); }
 
 function getLongestList(nestedList) {
