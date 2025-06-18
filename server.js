50ec9a14d64922f4fe7fbff2f0ab16c7a4e92399
Glitch (glottis) <none>
2025-06-18 14:16:14 -0400

    update a

--- server.js
+++ server.js
@@ -78,9 +78,9 @@ function hoverForeign(tokens) {
 }
 
 function matchCluster(phrase) {
-  let words = phrase.toLowerCase().split(" ").map((word) => [word, dict[word]]);
+  let words = phrase.toLowerCase().split(" ").map((word) => word in dict ? [word, dict[word]] : undefined);
+  if (words.includes(undefined)) return undefined; 
   for (let template of Object.keys(grammar["templates"])) {
-    console.log(words);
     if (words.length === template.split(" ").length && !template.split(" ").map((word, index) => word.at(0) != "[" && word === words[index][0] || word.at(0) === "[" && word.substring(1, word.length - 1) === words[index][1].pos).includes(false)) return grammar["templates"][template];
   }
   return undefined; 
