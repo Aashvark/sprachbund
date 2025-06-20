8e9304dc4630bc46add9b42d70a5ec7b69d88369
Glitch (glottis) <none>
2025-06-20 00:29:22 -0400

    update a

--- server.js
+++ server.js
@@ -69,7 +69,6 @@ function hoverForeign(tokens) {
     }
     else if (index < tokens.length - 1 && (stored + " " + tokens[next][0] in dict && !dict[stored + " " + tokens[next][0]].hint || matchCluster(stored + " " + tokens[next][0])) && token.length === 1) stored += " ";
     else {
-      console.log(stored);
       //let generated = generateKeys(stored);
       //string += formHints(token.length > 1 ? [stored, token[1]] : [stored], generated[0], generated[1]);
       stored = "";
@@ -79,7 +78,11 @@ function hoverForeign(tokens) {
 }
 
 function matchCluster(phrase) {
-  let words = phrase.toLowerCase().split(" ").map((word) => word in dict ? [word, dict[word]] : undefined);
+  let l = phrase.toLowerCase().split(" ");
+  let words = [];
+  for (let word of l) {
+    words.push(word in dict ? [word, dict[word]] : undefined);
+  }
   if (words.includes(undefined)) return undefined;
   for (template of Object.keys(grammar["templates"])) {
     if (words.length === template.split(" ").length && !template.split(" ").map((word, index) => word.at(0) != "[" && word === words[index][0] || word.at(0) === "[" && word.substring(1, word.length - 1) === words[index][1].pos).includes(false)) return grammar["templates"][template];
