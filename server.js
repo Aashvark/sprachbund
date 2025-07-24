f0a92a149e26042999cc8cb650e8075112012274
Sprachbund <none>
2025-07-23 22:02:39 -0400

    released

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ fastify.register(require("@fastify/static"), { root: path.join(__dirname, "publi
 fastify.register(require("@fastify/view"), { engine: { handlebars: handlebars } });
 fastify.register(require("@fastify/formbody"));
 
-handlebars.registerHelper('eq',  function (a, b)  { return a === b; });
+handlebars.registerHelper('eq',  function (a, b)  { return a == b; });
 
 handlebars.registerHelper('ifEquals',  function (a, b, options)  { return a === b ? options.fn(this) : options.inverse(this); });
 handlebars.registerHelper('ternaryEq', function (a, b, op1, op2) { return a === b ? op1 : op2; });
