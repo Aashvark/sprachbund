39e3c0dfc08d944bb98f19953b7cd04f6227e494
Sprachbund <none>
2025-07-28 12:56:51 -0400

    released

--- public/js/indicators.js
+++ public/js/indicators.js
@@ -1,14 +1,16 @@
-const fs = require("fs");
+async function update() {
+    for (let i = 0; i < document.getElementsByClassName("module"); i++) {
+        let module_id = document.getElementsByClassName("module")[i].getAttribute("data-id");
+        let length = await (await fetch(`/${module_id}`)).json().length;
+        console.log(module_id);
+        console.log(length);
 
-for (let i = 0; i < document.getElementsByClassName("module"); i++) {
-    let module_id = document.getElementsByClassName("module")[i].getAttribute("data-id");
-    let length = fs.readdirSync(`./public/json/${module_id}`).length;
-    console.log(module_id);
-    console.log(length);
-
-    for (let i = 0; i < length; i++) document.getElementsByClassName("completion")[i].innerHTML += `<div class="indicator" style="width: calc(${88 / length}% - 8px); margin: 0 1%;"></div>`;
-    document.getElementsByClassName("completion")[i].innerHTML += "<i class=\"fa-solid fa-star\"></i>";
+        for (let i = 0; i < length; i++) document.getElementsByClassName("completion")[i].innerHTML += `<div class="indicator" style="width: calc(${88 / length}% - 8px); margin: 0 1%;"></div>`;
+        document.getElementsByClassName("completion")[i].innerHTML += "<i class=\"fa-solid fa-star\"></i>";
+    }
 }
+
+update();
 
 // {{#if (eq lessons.length 1) includeZero=true}}
 // <div class="indicator" style="width: calc(89.5% - 8px);"></div>
