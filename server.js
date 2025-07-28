63011980940871b1d5313ee97b30f37bc989ef79
Sprachbund <none>
2025-07-28 13:43:21 -0400

    released

--- server.js
+++ server.js
@@ -193,19 +193,6 @@ fastify.get("/learn",   function (request, reply) { return reply.view("/src/lear
 fastify.get("/lesson",  function (request, reply) { return reply.redirect('/learn'); });
 fastify.post("/lesson",   function (request, reply) { return reply.view("/src/lesson.hbs", { seo: seo.index, id: `u${request.body.unit}-m${request.body.module}`, modlen: fs.readdirSync(`./public/json/u${request.body.unit}-m${request.body.module}`).length, lessons: require(`./public/json/u${request.body.unit}-m${request.body.module}/${request.body.lesson}.json`), isTest: request.body.lesson === "test" }); });
 
-let folders = fs.readdirSync("./public/json").filter(item => fs.statSync(path.join("./public/json", item)).isDirectory());
-console.log(folders.length);
-for (let i = 0; i < folders.length; i++) {
-  fastify.get(`/${folders[i]}`, (request, reply) => {
-    console.log(`/${folders[i]}`);
-    try {
-      console.log(fs.readdirSync(`./public/json/${folders[i]}`));
-      reply.send({ content: fs.readdirSync(`./public/json/${folders[i]}`) });
-    } 
-    catch (err) { reply.status(500).send({ error: 'Failed to read file' }); }
-  });
-}
-
 fastify.setNotFoundHandler(function(request, reply) { return reply.view("/src/error.hbs", { seo: seo.index, error: request.routeOptions.url }); });
 
 fastify.listen({ port: process.env.PORT || 4000, host: "0.0.0.0" },
