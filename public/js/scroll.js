8942cc4444273c22dc33e0bef4543fca3906b46c
Sprachbund <none>
2025-07-29 15:05:47 -0400

    released

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -3,7 +3,7 @@ document.onscroll = () => checkScroll();
 
 function checkScroll() {
     let scrollPos = window.scrollY;
-    for (let i = document.getElementsByClassName("divider").length - 1; i >= 0; i--) {
+    for (let i = 0; i < document.getElementsByClassName("divider").length; i++) {
         if (scrollPos + (document.getElementById("header").offsetHeight) >= document.getElementById("divider" + i).offsetTop) {
             document.getElementById("unitno").innerHTML = i;
             document.getElementById("unitdesc").innerHTML = document.getElementById("divider" + i).getAttribute("data-description");
