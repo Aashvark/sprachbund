920d27034b095500d08394fc3739ac4013130fa3
Sprachbund <none>
2025-07-29 11:58:43 -0400

    released

--- server.js
+++ server.js
@@ -188,12 +188,12 @@ function getLongestList(nestedList) {
 
 function escape(text) { return text ? text.replaceAll("'", "&#x27;") : text; }
 
-fastify.get("/",        function (request, reply) { return reply.view("/src/learn.hbs", { seo: seo.index, units: units }); });
-fastify.get("/learn",   function (request, reply) { return reply.view("/src/learn.hbs", { seo: seo.index, units: units }); });
+fastify.get("/",        function (request, reply) { return reply.view("/src/learn.hbs", { seo: seo, units: units }); });
+fastify.get("/learn",   function (request, reply) { return reply.view("/src/learn.hbs", { seo: seo, units: units }); });
 fastify.get("/lesson",  function (request, reply) { return reply.redirect('/learn'); });
-fastify.post("/lesson",   function (request, reply) { return reply.view("/src/lesson.hbs", { seo: seo.index, id: `u${request.body.unit}-m${request.body.module}`, modlen: fs.readdirSync(`./public/json/u${request.body.unit}-m${request.body.module}`).length, lessons: require(`./public/json/u${request.body.unit}-m${request.body.module}/${request.body.lesson}.json`), isTest: request.body.lesson === "test" }); });
+fastify.post("/lesson",   function (request, reply) { return reply.view("/src/lesson.hbs", { seo: seo, id: `u${request.body.unit}-m${request.body.module}`, modlen: fs.readdirSync(`./public/json/u${request.body.unit}-m${request.body.module}`).length, lessons: require(`./public/json/u${request.body.unit}-m${request.body.module}/${request.body.lesson}.json`), isTest: request.body.lesson === "test" }); });
 
-fastify.setNotFoundHandler(function(request, reply) { return reply.view("/src/error.hbs", { seo: seo.index, error: request.routeOptions.url }); });
+fastify.setNotFoundHandler(function(request, reply) { return reply.view("/src/error.hbs", { seo: seo, error: request.routeOptions.url }); });
 
 fastify.listen({ port: process.env.PORT || 4000, host: "0.0.0.0" },
   function (err, address) {
